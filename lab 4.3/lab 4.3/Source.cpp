1848f107838ba7a74ae0cdfe6900983565f80152
Sviatoslav-Valentyn <Sviatoslav-Valentyn.Hvozdyk.IT.2021@lpnu.ua>
2021-10-01 21:52:09 +0300

    first

---
+++ lab 4.3/lab 4.3/Source.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+int main()
+{
+	double xp, xk, dx, F, x, a, b, c;
+	cout << "a = "; cin >> a;
+	cout << "b = "; cin >> b;
+	cout << "c = "; cin >> c;
+	cout << "xp = "; cin >> xp;
+	cout << "xk = "; cin >> xk;
+	cout << "dx = "; cin >> dx;
+	cout << fixed;
+	cout << "---------------------------" << endl;
+	cout << "|" << setw(5) << "x" << " |"
+		<< setw(7) << "F" << " |" << endl;
+	cout << "---------------------------" << endl;
+	x = xp;
+	while (x <= xk)
+	{
+		if (c < 0 && b != 0)
+			F = a * pow(x, 2) + pow(b, 2) * x;
+		else
+			if (c > 0 && b == 0)
+				F = (x + a) / (x + c);
+			else
+				F = x / c;
+		cout << "|" << setw(7) << setprecision(2) << x
+			<< " |" << setw(10) << setprecision(3) << F
+			<< " |" << endl;
+		x += dx;
+	}
+	cout << "---------------------------" << endl;
+	return 0;
+}
